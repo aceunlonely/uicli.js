bada8d3e004eaa126395c189105437ccff6d1b9f
holyond <holyond@163.com>
2018-09-24 17:20:00 +0800

    part

---
+++ test/testLust/test3.js
@@ -0,0 +1,94 @@
+var lust = require('../../lust')
+
+var text = "???(string)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+var text = "???"
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+var text = "???[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+var text = "???(string)这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+var text = "???(string)[rue]" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+var text = "???这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+var text = "???(string)" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???[rue]" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(s)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(num)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(n)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(number)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(json)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(j)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(b)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(boolean)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(bool)[rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???()[]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???([rue]这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
+
+
+var text = "???(abdsf)这里填写你的名字" 
+console.log(text + "====================================")
+console.log(lust.solveLustValue(text))
